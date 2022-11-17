5fe2b7ac28b29e8a00abdda98faeaa1f059d8365
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2022-11-17 16:23:03 +0100

    My watch list app works, I need to add some front to it

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
+
 
 gem "autoprefixer-rails"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Destroying my DB...."
+
+Movie.destroy_all
+
+puts "Creating 20 movies.............."
+
+20.times do
+  Movie.create(title: Faker::JapaneseMedia::Naruto.character, overview: Faker::Lorem.paragraph, poster_url: "https://source.unsplash.com/random/1920x1080/?wallpaper,landscape", rating: Faker::Number.decimal(l_digits: 2))
+end
+
+
+puts "20 movies has created......."
